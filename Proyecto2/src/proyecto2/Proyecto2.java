da10a9200d333f87780f1d08f53cf0618943270d
KevinChamorro <60003927+kevinchamorro33@users.noreply.github.com>
2020-03-09 11:23:59 -0500

    Create Proyecto2.java
    Cambio

---
+++ Proyecto2/src/proyecto2/Proyecto2.java
@@ -0,0 +1,19 @@
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto2;
+
+/**
+ *
+ * @author Sistemas
+ */
+public class Proyecto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+        // TODO code application logic here
+    }
+    
+}
