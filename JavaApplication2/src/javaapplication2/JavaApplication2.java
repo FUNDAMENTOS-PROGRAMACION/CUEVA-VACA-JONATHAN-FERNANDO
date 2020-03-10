a9daf9b9f17f0e3e89a17c80785568cd6afea98d
Jonathan <60457701+Jona1804@users.noreply.github.com>
2020-03-10 11:55:04 -0500

    Update JavaApplication2.java
    Suma

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -14,13 +14,26 @@ public class JavaApplication2 {
     /**
      * @param args the command line arguments
      */
-    int numero=0;
+      int numero=0;
     boolean Valorboolean=true;
     char letra='u' ;
     double numeroc= 0.1;
     
+    
     public static void main(String[] args) {
         // TODO code application logic here
-    }
-    
+    implimirNombre ();
+    sumar2Numeros(1,2);
+    System.out.println(sumar2Numeros(1,4));
+     }
+ 
+  public static void implimirNombre () {
+  System.out.println("Jonathan Fernando Cueva Vaca");
+  }
+  public static int sumar2Numeros (int num1, int num2){
+    int suma = num1 + num2;
+      return suma;
+  
+  }
+  
 }
