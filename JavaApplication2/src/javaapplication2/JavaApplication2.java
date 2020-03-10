173afd8cc996d9d3107624cef487ebcccc83de24
Jonathan <60457701+Jona1804@users.noreply.github.com>
2020-03-10 11:08:51 -0500

    Create JavaApplication2.java
    Caracteres

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Jhons
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    int numero=0;
+    boolean Valorboolean=true;
+    char letra='u' ;
+    double numeroc= 0.1;
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
